3327f591231cf4d7678076e140ebca9ac56a3c7b
David McFadzean <davidmc@gmail.com>
2023-08-19 14:43:30 -0400

    Inital layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import { Grid, Typography, ToggleButton, ToggleButtonGroup } from '@mui/material';
 
 function App() {
+  const [selectedButton, setSelectedButton] = React.useState("mu");
+
+  const handleButtonChange = (event, newButton) => {
+    setSelectedButton(newButton);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Grid container direction="column" justifyContent="center" alignItems="center" spacing={2}>
+      <Grid item>
+        <Typography variant="h4">Title</Typography>
+      </Grid>
+      <Grid item>
+        <Typography>A long question goes here</Typography>
+      </Grid>
+      <Grid item>
+        <ToggleButtonGroup
+          value={selectedButton}
+          exclusive
+          onChange={handleButtonChange}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <ToggleButton value="yes" sx={{ marginRight: 1, textTransform: 'none' }}>
+            <Typography>
+              Yes<br />
+              I agree<br />
+              Affirmative
+            </Typography>
+          </ToggleButton>
+          <ToggleButton value="mu" sx={{ marginRight: 1, textTransform: 'none' }}>            
+          <Typography>
+              I don't understand<br />
+              or I don't know<br />
+              or I'm indifferent
+            </Typography>
+          </ToggleButton>
+          <ToggleButton value="no" sx={{ textTransform: 'none' }}>            
+          <Typography>
+              No<br />
+              I disagree<br />
+              Negative
+            </Typography>
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </Grid>
+      <Grid item>
+        <Typography>a countdown timer</Typography>
+      </Grid>
+    </Grid>
   );
 }
 
